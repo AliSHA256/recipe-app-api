53cafb154e3903b3968a9b58ea9a7120a2b566db
Ali Shafiei <alishfieimce@gmail.com>
2023-11-12 18:53:43 -0500

    checking args.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -34,3 +34,9 @@ class AdminSitesTests(TestCase):
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
+    def test_edit_user_page(self):
+        """Test the edit_user_page works."""
+        url = reverse('admin:core_user_change', args[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
