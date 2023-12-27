16ddf6414a3679bc8185596b155aec6374956c12
Ali Shafiei <alishfieimce@gmail.com>
2023-12-26 23:16:24 -0500

    Additional TestCases for recipe's APIs and changing view and creating tags models.

---
+++ app/recipe/serializers.py
@@ -0,0 +1,23 @@
+"""
+Serializers for recipe APIs.
+"""
+from rest_framework import serializers
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializers for recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'link', 'time_minutes', 'price']
+        read_only_fields = ['id']
+
+
+class RecipeDetailsSerializer(RecipeSerializer):
+    """
+    Serializer for recipes detail view.
+    """
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
