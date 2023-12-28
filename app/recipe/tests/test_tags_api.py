5f3fe593036710d3e8d56dabbb46db1a5ada0020
Ali Shafiei <alishfieimce@gmail.com>
2023-12-27 23:08:52 -0500

    creating test for adding and updating tags via recipe's API.

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -17,6 +17,13 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """
+    Create and return a tag detail urL.
+    """
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_user(email='user@example.com', password='pass123'):
     """Create and return new user."""
     return get_user_model().objects.create_user(email=email, password=password)
@@ -40,9 +47,9 @@ class PrivateTagsApiTests(TestCase):
     def setUp(self):
         self.user = create_user()
         self.client = APIClient()
-        self.client.force_authenticated(self.user)
+        self.client.force_authenticate(self.user)
 
-    def test_retrieve_tage(self):
+    def test_retrieve_tag(self):
         """Test authorized user retrieving tags."""
         Tag.objects.create(user=self.user, name='vegan')
         Tag.objects.create(user=self.user, name='dessert')
@@ -65,3 +72,25 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='juicy')
+        payload = {
+            'name': 'after waterloo',
+        }
+
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting a tag."""
+        tag = Tag.objects.create(user=self.user, name='juicy')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Tag.objects.filter(id=tag.id).exists())
